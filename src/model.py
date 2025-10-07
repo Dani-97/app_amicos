8bd1932c576251781f365f40563f56fd2ab09f02
Dani-97 <daniel.iglesias.moris@outlook.es>
2025-10-07 09:27:30 +0200

    Subida de los test de los casos de uso de gestión de perfiles.

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import os
 from passlib.hash import pbkdf2_sha256
+import re
 import sqlite3
 
 class User:
@@ -16,9 +17,39 @@ class User:
         return self.email
 
     def get_role(self):
-        return self.role
+        return self.role 
 
 class LogInException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class InvalidUsernameException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class InvalidEmailException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class InvalidPasswordException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class DuplicatedUserException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class UserDeletionException(Exception):
 
     def __init__(self, message):
         self.message = message
@@ -27,52 +58,146 @@ class LogInException(Exception):
 class Model:
 
     def __init__(self):
-        self.DB_PATH = "users.db"
+        self.USERS_DB_PATH = "users.db"
         self.users_list = []
         self.current_user = None
 
     def init_db(self):
-        conn = sqlite3.connect(self.DB_PATH)
+        conn = sqlite3.connect(self.USERS_DB_PATH)
         c = conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS users (
                         username TEXT PRIMARY KEY,
                         email TEXT NOT NULL,
                         role TEXT NOT NULL,
-                        password_hash TEXT NOT NULL
+                        password_hash TEXT NOT NULL,
+                        is_logged_in BOOLEAN NOT NULL
                     )''')
         conn.commit()
         conn.close()
 
     def remove_db_file(self):
-        os.remove(self.DB_PATH)
+        os.remove(self.USERS_DB_PATH)
+
+    def username_format_validator(self, username):
+        format_validation = (len(username.strip())>=4) and (len(username.strip())<=12)
+        format_validation = format_validation and (username.find(" ")==-1)
+
+        return format_validation
+
+    def email_format_validator(self, email):
+        reg = r"^\S+@\S+\.\S+$"
+        pattern = re.compile(reg)
+        patt_match = re.search(pattern, email)
+
+        return patt_match
+
+    def password_format_validator(self, password):
+        reg = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#%])[A-Za-z\d@$#%]{6,12}$"
+        pattern = re.compile(reg)
+        patt_match = re.search(pattern, password)
+
+        return patt_match
+
+    def get_user_by_username(self, username):
+        conn = sqlite3.connect(self.USERS_DB_PATH)
+        c = conn.cursor()
+        c.execute("SELECT username, email, role, is_logged_in FROM users WHERE username=?", (username,))
+        result = c.fetchone()
+
+        if (result):
+            return result
+        
+        return None
 
     def create_user(self, username, email, role, password):
-        conn = sqlite3.connect(self.DB_PATH)
-        c = conn.cursor()
-        hashed = pbkdf2_sha256.hash(password)
-        try:
-            c.execute("INSERT INTO users (username, email, role, password_hash) VALUES (?, ?, ?, ?)", (username, email, role, hashed))
-            conn.commit()
-            return True
-        except sqlite3.IntegrityError:
-            return False
-        finally:
-            conn.close()
+        is_username_format_correct = self.username_format_validator(username)
+        is_email_format_correct = self.email_format_validator(email)
+        is_password_format_correct = self.password_format_validator(password)
+
+        if (not is_username_format_correct):
+            raise InvalidUsernameException("The username format is not correct. It must be between 4 and 12 characters.")
+
+        if (not is_email_format_correct):
+            raise InvalidEmailException("The email format is not correct")
+
+        if (not is_password_format_correct):
+            raise InvalidPasswordException("The password must be between 6 and 12 characters, have at least one number, one uppercase, one lowercase and one special character from $, @, # or %.")
+
+        retrieved_user = self.get_user_by_username(username)
+        if (retrieved_user is not None):
+            raise DuplicatedUserException("This username already exists")
+        else:
+            conn = sqlite3.connect(self.USERS_DB_PATH)
+            c = conn.cursor()
+
+            hashed = pbkdf2_sha256.hash(password)
+            is_logged_in = False
+            try:
+                c.execute("INSERT INTO users (username, email, role, password_hash, is_logged_in) VALUES (?, ?, ?, ?, ?)", (username, email, role, hashed, is_logged_in))
+                conn.commit()
+                return True
+            except sqlite3.IntegrityError:
+                return False
+            finally:
+                conn.close()
 
     def login_user(self, username, typed_password):
-        conn = sqlite3.connect(self.DB_PATH)
+        conn = sqlite3.connect(self.USERS_DB_PATH)
         c = conn.cursor()
         c.execute("SELECT * FROM users WHERE username=?", (username,))
         result = c.fetchone()
-        conn.close()
+        
         if (result):
-            username, email, role, stored_password = result
+            username, email, role, stored_password, is_logged_in = result
             if (pbkdf2_sha256.verify(typed_password, stored_password)):
                 self.current_user = User(username, email, role)
-                
+                c.execute("UPDATE users SET is_logged_in=? WHERE username=?", (True, username,))
+                conn.commit()
+                conn.close()
+
                 return self.current_user
+
+        conn.close()
 
         raise LogInException("The username or the password are incorrect")
 
+    def logout_current_user(self):
+        conn = sqlite3.connect(self.USERS_DB_PATH)
+        c = conn.cursor()
+        c.execute("UPDATE users SET is_logged_in=? WHERE username=?", (False, self.current_user.get_username(),))
+        conn.commit()
+
+        conn.close()
+
+        self.current_user = None
+
+    def delete_user(self, username, email):
+        conn = sqlite3.connect(self.USERS_DB_PATH)
+        c = conn.cursor()
+        c.execute("SELECT * FROM users WHERE username=? and email=?", (username, email))
+        result = c.fetchone()
+
+        if (result):
+            c.execute("DELETE FROM users WHERE username=?", (username,))
+        else:
+            conn.close()
+            raise UserDeletionException("The email is not correct")
+
+        conn.commit()
+        conn.close()
+
     def get_current_user(self):
+        conn = sqlite3.connect(self.USERS_DB_PATH)
+        c = conn.cursor()
+        c.execute("SELECT username, email, role, is_logged_in FROM users")
+        result = c.fetchall()
+
+        self.current_user = None
+        for current_user in result:
+            username, email, role, is_logged_in = current_user
+            if (is_logged_in):
+                self.current_user = User(username, email, role)                
+
+        conn.close()
+
         return self.current_user
--- src/profile_manager_test.py
+++ src/profile_manager_test.py
@@ -1,39 +1,348 @@
 import unittest
-from model import Model, User, LogInException
+from model import Model, User
+from model import InvalidUsernameException, InvalidEmailException, InvalidPasswordException
+from model import LogInException, UserDeletionException, DuplicatedUserException
 
 class SignupTestCase(unittest.TestCase):
 
-    def test_correct_signup_and_login(self):
+    # Creación de cuenta correcta.
+    def test_cp1(self):
         model = Model()
         model.init_db()
 
-        created_user = User("user1", "user1@app.com", "Usuario común")
-        password = "1234"
+        user1 = User("user1", "user1@app.com", "Usuario común")
+        user2 = User("user2", "user2@app.com", "Logopeda")
+        user3 = User("user3", "user3@app.com", "Cuidador")
 
-        model.create_user(created_user.get_username(), 
-                          created_user.get_email(),
-                          created_user.get_role(),
-                          password)
-        retrieved_user = model.login_user(created_user.get_username(), password)
-        self.assertEqual(created_user.get_username(), retrieved_user.get_username())
-        self.assertEqual(created_user.get_email(), retrieved_user.get_email())
-        self.assertEqual(created_user.get_role(), retrieved_user.get_role())
+        users_list = [user1, user2, user3]
+
+        password = "aA12345678@"
+
+        for user_aux in users_list:
+            model.create_user(user_aux.get_username(), 
+                            user_aux.get_email(),
+                            user_aux.get_role(),
+                            password)
+
+        for user_aux in users_list:
+            retrieved_user = model.login_user(user_aux.get_username(), password)
+            self.assertEqual(user_aux.get_username(), retrieved_user.get_username())
+            self.assertEqual(user_aux.get_email(), retrieved_user.get_email())
+            self.assertEqual(user_aux.get_role(), retrieved_user.get_role())
 
         model.remove_db_file()
 
-    def test_incorrect_login(self):
+    # Inicio de sesión correcto.
+    def test_cp2(self):
+        model = Model()
+        model.init_db()
+
+        user1 = User("user1", "user1@app.com", "Usuario común")
+        user2 = User("user2", "user2@app.com", "Logopeda")
+        users_list = [user1, user2]
+
+        password = "aA12345678@"
+
+        for user_aux in users_list:
+            model.create_user(user_aux.get_username(), 
+                            user_aux.get_email(),
+                            user_aux.get_role(),
+                            password)
+
+        for user_aux in users_list:
+            _ = model.login_user(user_aux.get_username(), password)
+            current_user = model.get_current_user()
+
+            self.assertEqual(current_user.get_username(), user_aux.get_username())
+            self.assertEqual(current_user.get_email(), user_aux.get_email())
+            self.assertEqual(current_user.get_role(), user_aux.get_role())
+
+        model.remove_db_file()
+
+    # Eliminación de cuenta correcto.
+    def test_cp3(self):
+        model = Model()
+        model.init_db()
+
+        user1 = User("user1", "user1@app.com", "Usuario común")
+        password = "aA12345678@"
+
+        model.create_user(user1.get_username(), 
+                        user1.get_email(),
+                        user1.get_role(),
+                        password)
+        _ = model.login_user(user1.get_username(), password)
+        current_user = model.get_current_user()
+
+        self.assertEqual(current_user.get_username(), user1.get_username())
+        self.assertEqual(current_user.get_email(), user1.get_email())
+        self.assertEqual(current_user.get_role(), user1.get_role())
+
+        model.delete_user(current_user.get_username(), current_user.get_email())
+
+        with self.assertRaises(LogInException):
+            _ = model.login_user(user1.get_username(), password)
+
+        model.remove_db_file()
+
+    # Cierre de sesión correcto.
+    def test_cp4(self):
+        model = Model()
+        model.init_db()
+
+        user1 = User("user1", "user1@app.com", "Usuario común")
+        password = "aA12345678@"
+        model.create_user(user1.get_username(), 
+                        user1.get_email(),
+                        user1.get_role(),
+                        password)
+        _ = model.login_user(user1.get_username(), password)
+
+        current_user = model.get_current_user()
+
+        self.assertEqual(current_user.get_username(), user1.get_username())
+        self.assertEqual(current_user.get_email(), user1.get_email())
+        self.assertEqual(current_user.get_role(), user1.get_role())
+
+        model.logout_current_user()
+        current_user = model.get_current_user()
+
+        self.assertEqual(current_user, None)
+
+        model.remove_db_file()
+
+    # Creación de usuario: nombre con formato incorrecto.
+    def test_cp1_1(self):
+        model = Model()
+        model.init_db()
+
+        password = "aA12345678@"
+        user1 = User("", "user1@app.com", "Usuario común")
+
+        with self.assertRaises(InvalidUsernameException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            password)
+
+        user1 = User("Use", "user1@app.com", "Usuario común")
+
+        with self.assertRaises(InvalidUsernameException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            password)
+
+        user1 = User("UserUserUserUser", "user1@app.com", "Usuario común")
+
+        with self.assertRaises(InvalidUsernameException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            password)
+
+        user1 = User("U s e r s", "user1@app.com", "Usuario común")
+
+        with self.assertRaises(InvalidUsernameException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            password)
+
+        model.remove_db_file()
+
+    # Creación de usuario: usuario duplicado.
+    def test_cp1_2(self):
+        model = Model()
+        model.init_db()
+
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        user2 = User("testing_user", "user2@app.com", "Logopeda")
+        correct_password = "aA12345678@"
+        
+        model.create_user(user1.get_username(), 
+                    user1.get_email(),
+                    user1.get_role(),
+                    correct_password)
+
+        with self.assertRaises(DuplicatedUserException):
+            model.create_user(user2.get_username(), 
+                        user2.get_email(),
+                        user2.get_role(),
+                        correct_password)
+
+        model.remove_db_file()
+
+    # Creación de usuario: email con formato incorrecto.
+    def test_cp1_3(self):
+        model = Model()
+        model.init_db()
+
+        user1 = User("testing_user", "user1app.com", "Usuario común")
+        correct_password = "aA12345678@"
+
+        with self.assertRaises(InvalidEmailException):
+            model.create_user(user1.get_username(), 
+                    user1.get_email(),
+                    user1.get_role(),
+                    correct_password)
+
+        user1 = User("testing_user", "user1@app", "Usuario común")
+        correct_password = "aA12345678@"
+
+        with self.assertRaises(InvalidEmailException):
+            model.create_user(user1.get_username(), 
+                    user1.get_email(),
+                    user1.get_role(),
+                    correct_password)
+
+        model.remove_db_file()
+
+    # Creación de usuario: contraseña con formato incorrecto.
+    def test_cp_1_4(self):
+        model = Model()
+        model.init_db()
+
+        # Menos de 6 caracteres.
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        correct_password = "aA1@"
+
+        with self.assertRaises(InvalidPasswordException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            correct_password)
+
+        # Más de 12 caracteres.
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        correct_password = "aA1234567890@"
+
+        with self.assertRaises(InvalidPasswordException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            correct_password)
+
+        # No tiene, al menos, un número.
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        correct_password = "aABCDEFG@"
+
+        with self.assertRaises(InvalidPasswordException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            correct_password)
+
+        # No tiene, al menos, una mayúscula.
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        correct_password = "aabcdefg@"
+
+        with self.assertRaises(InvalidPasswordException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            correct_password)
+
+        # No tiene, al menos, una minúscula.
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        correct_password = "AABCDEFG@"
+
+        with self.assertRaises(InvalidPasswordException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            correct_password)
+
+        # No tiene, al menos, un carácter especial.
+        user1 = User("testing_user", "user1@app.com", "Usuario común")
+        correct_password = "aA12345678"
+
+        with self.assertRaises(InvalidPasswordException):
+            model.create_user(user1.get_username(), 
+                            user1.get_email(),
+                            user1.get_role(),
+                            correct_password)
+
+        model.remove_db_file()
+
+    # Inicio de sesión: nombre de usuario incorrecto.
+    def test_cp_2_1(self):
+        model = Model()
+        model.init_db()
+
+        correct_username = "user1"
+        wrong_username = "user2"
+        created_user = User(correct_username, "user1@app.com", "Usuario común")
+        correct_password = "aA12345678@"
+        model.create_user(created_user.get_username(), 
+                    created_user.get_email(),
+                    created_user.get_role(),
+                    correct_password)
+
+        with self.assertRaises(LogInException):
+            model.login_user(wrong_username, correct_password)
+
+        model.remove_db_file()
+
+    # Inicio de sesión: contraseña incorrecta.
+    def test_cp_2_2(self):
         model = Model()
         model.init_db()
 
         created_user = User("user1", "user1@app.com", "Usuario común")
-        correct_password = "1234"
+        correct_password = "aA12345678@"
         wrong_password = "123"
         model.create_user(created_user.get_username(), 
                           created_user.get_email(),
                           created_user.get_role(),
                           correct_password)
+
         with self.assertRaises(LogInException):
             model.login_user(created_user.get_username(), wrong_password)
+
+        model.remove_db_file()
+
+    # Inicio de sesión: usuario no logueado.
+    def test_cp_2_3(self):
+        model = Model()
+        model.init_db()
+
+        current_user = model.get_current_user()
+        self.assertEqual(current_user, None)
+
+        created_user = User("user1", "user1@app.com", "Usuario común")
+        correct_password = "aA12345678@"
+        model.create_user(created_user.get_username(), 
+                    created_user.get_email(),
+                    created_user.get_role(),
+                    correct_password)
+
+        current_user = model.get_current_user()
+        self.assertEqual(current_user, None)
+
+        model.remove_db_file()
+
+    # Inicio de sesión: email incorrecto.
+    def test_cp_3_1(self):
+        model = Model()
+        model.init_db()
+
+        current_user = model.get_current_user()
+        self.assertEqual(current_user, None)
+
+        correct_email = "user1@app.com"
+        incorrect_email = "user2@app.com"
+        created_user = User("user1", "user1@app.com", "Usuario común")
+        correct_password = "aA12345678@"
+        model.create_user(created_user.get_username(), 
+                    created_user.get_email(),
+                    created_user.get_role(),
+                    correct_password)
+
+        model.login_user(created_user.get_username(), correct_password)
+
+        with self.assertRaises(UserDeletionException):
+            model.delete_user(created_user.get_username(), incorrect_email)
 
         model.remove_db_file()
 
