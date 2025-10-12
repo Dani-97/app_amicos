de100893e5074e2ef6e25fa3a1fc2a2b0022a910
Dani-97 <daniel.iglesias.moris@outlook.es>
2025-10-12 19:20:13 +0200

    Aplicación implementada con Kivy que funciona de manera satisfactoria en Android y permite visualizar el vídeo de la cámara.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,67 @@
-from providers import UIAssetsManager
-from providers import MessageBuilderProvider
-from providers import Settings_Provider
-from profiles_manager import ProfilesManagerProvider
-from views import AmicosApp
+from android.permissions import request_permissions, Permission
+from kivy.app import App
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics.texture import Texture
+from kivy.uix.camera import Camera
+from kivy.lang import Builder
+from jnius import autoclass
+import numpy as np
+import cv2
+
+Config.set('kivy', 'camera', 'opencv')
+
+request_permissions([
+    Permission.CAMERA,
+    Permission.WRITE_EXTERNAL_STORAGE,
+    Permission.READ_EXTERNAL_STORAGE
+])
+
+CameraInfo = autoclass('android.hardware.Camera$CameraInfo')
+CAMERA_INDEX = {'front': CameraInfo.CAMERA_FACING_FRONT, 'back': CameraInfo.CAMERA_FACING_BACK}
+Builder.load_file("myapplayout.kv")
+
+
+class AndroidCamera(Camera):
+    resolution = (640, 480)
+    index = CAMERA_INDEX['back']
+    counter = 0
+
+    def on_tex(self, *l):
+        if self._camera._buffer is None:
+            return None
+
+        super(AndroidCamera, self).on_tex(*l)
+        self.texture = Texture.create(size=np.flip(self.resolution), colorfmt='rgb')
+        frame = self.frame_from_buf()
+        self.frame_to_screen(frame)
+
+    def frame_from_buf(self):
+        w, h = self.resolution
+        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
+        frame_bgr = cv2.cvtColor(frame, 93)
+        if self.index:
+            return np.flip(np.rot90(frame_bgr, 1), 1)
+        else:
+            return np.rot90(frame_bgr, 3)
+
+    def frame_to_screen(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.putText(frame_rgb, str(self.counter), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        self.counter += 1
+        flipped = np.flip(frame_rgb, 0)
+        buf = flipped.tobytes()
+        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
+
+
+class MyLayout(BoxLayout):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        return MyLayout()
+
 
 if __name__ == '__main__':
-    message_builder_provider = MessageBuilderProvider()
-    profiles_manager_provider = ProfilesManagerProvider()
-    profiles_manager_provider.init_db()
-    settings_provider = Settings_Provider()
-    ui_assets_manager = UIAssetsManager()
-    
-    amicos_app = AmicosApp()
-    amicos_app.set_ui_assets_manager(ui_assets_manager)
-    amicos_app.set_message_builder_provider(message_builder_provider)
-    amicos_app.set_profiles_manager_provider(profiles_manager_provider)
-    amicos_app.set_settings_provider(settings_provider)
-
-    amicos_app.run()
+    MyApp().run()
